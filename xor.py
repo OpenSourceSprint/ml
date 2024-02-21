63e7217831f2061a0e7bd2c6b6001144f4c2f5fe
भव्य / bhavya <59764117+bhqv@users.noreply.github.com>
2024-02-22 00:45:07 +0530

    Add files via upload

---
+++ xor.py
@@ -0,0 +1,65 @@
+import numpy as np
+
+# XOR Gate #
+
+def sig(x):
+    return 1 / (1 + np.exp(x)) 
+
+def sigDeriv(x):
+    return x * (1 - x)
+
+
+input = np.array([[0,0],[0,1],[1,0],[1,1]])
+target = np.array([[0],[1],[1],[0]])
+
+inputLayerNeurons, hiddenLayerNeurons, outputLayerNeurons = 2, 2, 1
+
+# Init random weights
+hiddenWeights = np.random.uniform(size=(inputLayerNeurons,hiddenLayerNeurons))
+hiddenBias = np.zeros((1, 2), dtype = float)
+outputWeights = np.random.uniform(size=(hiddenLayerNeurons,outputLayerNeurons))
+outputBias = np.zeros((1, 1), dtype = float)
+
+epochs = 50000
+lRate = 1 
+
+
+for _ in range(epochs):
+    
+    # forward prop 
+    hiddenLayerActivation = np.dot(input, hiddenWeights)
+    hiddenLayerActivation += hiddenBias
+    hiddenLayerOutput = sig(hiddenLayerActivation)
+
+    outputLayerActivation = np.dot(hiddenLayerOutput, outputWeights)
+    outputLayerActivation += outputBias
+    predictedOutput = sig(outputLayerActivation)
+
+    # back prop
+    error = target 
+    dPredictedOutput = error
+
+    errorHiddenLayer = dPredictedOutput.dot(outputWeights.T)
+    dHiddenLayer = errorHiddenLayer * sigDeriv(hiddenLayerOutput)
+    
+    
+    # updating weights, bias
+    outputWeights += hiddenLayerOutput.T.dot(dPredictedOutput) * lRate
+    outputBias += np.sum(dPredictedOutput, axis = 0, keepdims=True) 
+    hiddenWeights += input.T.dot(dHiddenLayer)
+    hiddenBias += np.sum(dHiddenLayer, axis = 0, keepdims=True) * lRate
+    
+
+print("Final hidden weights: ", end = '')
+print(*hiddenWeights)
+print("Final hidden bias: ", end = '')
+print(*hiddenBias)
+print("Final output weights: ", end = '')
+print(*outputWeights)
+print("Final output bias: ", end = '')
+print(*outputBias)
+
+
+print("\nOutput from nn: ", end = '')
+print(*predictedOutput)
+
