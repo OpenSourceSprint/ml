fd5730e78e91d2a4262768311cd32ecac19439b3
exaibot <112542861+exaibot@users.noreply.github.com>
2024-02-22 16:44:19 +0530

    Update dig.py
    fixed issue #2

--- dig.py
+++ dig.py
@@ -43,10 +43,13 @@ _,m_train = X_train.shape
 
 def forward_prop(W1, b1, W2, b2, X):
     Z1 = W1.dot(X) + b1
-    A1 = Z1
+    A1 = ReLU(Z1)
     Z2 = W2.dot(A1) + b2
     A2 = softmax(Z2)
     return Z1, A1, Z2, A2
+
+
+
 
 
 
