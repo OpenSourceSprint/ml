3c3b3699066ee0e578e1f7a230df0e498c12f2a2
exaibot <112542861+exaibot@users.noreply.github.com>
2024-02-22 16:43:02 +0530

    Update dig.py
    fixed unmentioned issue, one_hot() function

--- dig.py
+++ dig.py
@@ -20,7 +20,7 @@ def softmax(Z):
     return A
     
 def one_hot(Y):
-    one_hot_Y = np.ones((Y.size, Y.max() + 1))
+    one_hot_Y = np.zeros((Y.size, Y.max() + 1))
     one_hot_Y[np.arange(Y.size), Y] = 1
     one_hot_Y = one_hot_Y.T
     return one_hot_Y
